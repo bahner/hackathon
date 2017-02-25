51c898be6f0ebfddb644501538709c8b92e7e579
Lars Bahner <lars.bahner@gmail.com>
2017-02-25 15:53:11 +0100

    Fixed syntax error

--- bahner/radiotest.py
+++ bahner/radiotest.py
@@ -2,10 +2,51 @@ from microbit import *
 import radio
 import random
 
+# INIT
 spillere = set()
 
 radio.config(group=213)
 radio.on()
+
+# INIT gamers
+def _init():
+    """Initialize game"""
+
+    min_id = random.randint(0, 10000)
+    spillere.add(min_id)
+
+    while True:
+        display.show(str(len(spillere)))
+        msg = radio.receive()
+        if msg:
+            if msg == 'init':
+                break
+            else:
+                spillere.add(msg)
+        if button_a.is_pressed():
+            radio.send(str(min_id))
+        if button_b.is_pressed():
+            for i in range(10):
+                radio.send('init')
+            break
+
+
+
+
+
+
+
+
+
+
+
+
+
+while True:
+    if button_a.is_pressed():
+        display.clear()
+        send_random_pos(vis=button_b.is_pressed())
+    
 
 def send_random_pos(vis=False):
     """Sender en ttildelfig posisjon
@@ -16,29 +57,3 @@ def send_random_pos(vis=False):
     if vis:
         display.set_pixel(x, y, 5)
     radio.send("%s,%s" % (x, y))
-
-
-min_id = random.randint(0, 10000)
-spillere.add(min_id)
-
-# INIT gamers
-while True:
-    display.show(str(len(spillere)))
-    msg = radio.receive()
-    if msg:
-        if msg == 'init':
-            break
-        else:
-            spillere.add(msg)
-    if button_a.is_pressed():
-        radio.send(str(min_id))
-    if button_b.is_pressed():
-        for i in range(10):
-            radio.send('init')
-        break
-
-while True:
-    if button_a.is_pressed():
-        display.clear()
-        send_random_pos(vis=button_b.is_pressed())
-    
