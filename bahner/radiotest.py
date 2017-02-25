e4561bf5784a5bc6f7c372038ded0e270b07db57
Lars Bahner <lars.bahner@gmail.com>
2017-02-25 15:33:37 +0100

    init game

--- bahner/radiotest.py
+++ bahner/radiotest.py
@@ -2,6 +2,10 @@ from microbit import *
 import radio
 import random
 
+spillere = set()
+
+radio.config(group=213)
+radio.on()
 
 def send_random_pos(vis=False):
     """Sender en ttildelfig posisjon
@@ -13,10 +17,27 @@ def send_random_pos(vis=False):
         display.set_pixel(x, y, 5)
     radio.send("%s,%s" % (x, y))
 
+def min_id = random.randint(0, 10000)
+spillere.add(min_id)
 
-radio.config(group=213)
-radio.on()
+# INIT gamers
+while True:
+    msg = radio.recv():
+    if msg:
+        if msg == 'init':
+            break
+        else:
+            spillere.add(msg)
+        if button_a.is_pressed():
+            radio.send(min_id)
+        display.show(len(spillere))
+    if button_b.is_pressed():
+        for i in range(10):
+            radio.send('init)
+        break
+
 while True:
     if button_a.is_pressed():
         display.clear()
         send_random_pos(vis=button_b.is_pressed())
+    
