2100704a6f9e652581e39985ae1d005ae3e19e59
Job Nestor Bahner <job@phoenix.home.bahner.com>
2017-02-25 15:05:33 +0100

    done all this. Ny shizz.

--- job/fil.py
+++ job/fil.py
@@ -1,3 +1,17 @@
+from microbit import *
+import radio
 
+# Virker en radio hvis den ikke er på?
+radio.on()
 
-print("hello, world!")
+radio.config(group=213)
+
+# Luuuup
+while True:
+    pos = radio.receive()
+    if pos:
+        display.clear()
+        x,y=pos.split(',')
+        display.set_pixel(int(x),int(y),9)
+        
+
